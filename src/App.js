a529a13224defd29e7ab7bb7405ecb3d87762434
RagunathRa <ragunath065@gmail.com>
2023-09-29 20:26:55 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react'
+// import CRUDRouter from './myProjects/CRUDRouter'
+import Router1 from './myProjects/Router1'
+
+const App = () => {
+  return (
+    <div>
+      <Router1/>
+      {/* <CRUDRouter/> */}
+    </div>
+  )
+}
+
+export default App
